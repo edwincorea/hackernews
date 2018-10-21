5767eddef5dcb69b82ecd7d4ab77cf42e0f43e7a
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-10-21 11:09:02 -0600

    Paginated Fetch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 require('./App.css')
 
 const DEFAULT_QUERY = 'redux'
+const DEFAULT_HPP = '100'
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
 const PARAM_SEARCH = 'query='
+const PARAM_PAGE = 'page='
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
 
@@ -24,11 +27,24 @@ class App extends Component {
 	}
 
 	setSearchTopStories = (result) => {
-		this.setState({ result })
+		const { hits, page } = result
+		
+		const oldHits = page !== 0
+			? this.state.result.hits
+			: []
+
+		const updatedHits = [
+			...oldHits,
+			...hits
+		]
+
+		this.setState({ 
+			result: { hits: updatedHits, page}
+		 })
 	}
 
-	fetchSearchTopStories = (searchTerm) => {
-		const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`
+	fetchSearchTopStories = (searchTerm, page = 0) => {
+		const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
 		fetch(url)
 			.then(response => response.json())
 			.then(result => this.setSearchTopStories(result))
@@ -55,24 +71,30 @@ class App extends Component {
 
 	render() {
 		const { searchTerm, result } = this.state
+		const page = (result && result.page) || 0
 		return (
 			<div className="page">
 				<div className="interactions">
-					<Search
+			  		<Search
 						value={searchTerm}
 						onChange={this.onSearchChange}
 						onSubmit={this.onSearchSubmit}
-					>
-            			Search
-					</Search>
+			  		>
+						Search
+			  		</Search>
 				</div>
 				{ result &&
-          <Table
-          	list={result.hits}
-          	onDismiss={this.onDismiss}
-          />
+					<Table
+						list={result.hits}
+						onDismiss={this.onDismiss}
+					/>
 				}
-			</div>
+				<div className="interactions">
+			  		<Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+						More...
+			  		</Button>
+				</div>
+		  </div>
 		)
 	}
 }
