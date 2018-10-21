4e9d6ae1c473b435f5074d8259bdc69c7d1bdf8f
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-10-21 10:21:43 -0600

    Client- or Server-side Search.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
 const PARAM_SEARCH = 'query='
 
-const isSearched = (searchTerm) => (item) =>
-	item.title.toLowerCase().includes(searchTerm.toLowerCase())
-
 class App extends Component {
 
 	constructor(props) {
@@ -19,105 +16,111 @@ class App extends Component {
 			result: null,
 			searchTerm: DEFAULT_QUERY,
 		}
-
-		this.setSearchTopstories = this.setSearchTopstories.bind(this)
-		this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this)
-		this.onSearchChange = this.onSearchChange.bind(this)
-		this.onDismiss = this.onDismiss.bind(this)
-	}
-
-	setSearchTopstories(result) {
-		this.setState({ result })
-	}
-
-	fetchSearchTopstories(searchTerm) {
-		const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`
-		fetch(url)
-			.then(response => response.json())
-			.then(result => this.setSearchTopstories(result))
-			.catch(e => console.log(e))
 	}
 
 	componentDidMount() {
 		const { searchTerm } = this.state
-		this.fetchSearchTopstories(searchTerm)
+		this.fetchSearchTopStories(searchTerm)
 	}
 
-	onSearchChange(event) {
+	setSearchTopStories = (result) => {
+		this.setState({ result })
+	}
+
+	fetchSearchTopStories = (searchTerm) => {
+		const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`
+		fetch(url)
+			.then(response => response.json())
+			.then(result => this.setSearchTopStories(result))
+			.catch(e => console.log(e))
+	}
+
+	onSearchChange = (event) => {
 		this.setState({ searchTerm: event.target.value })
 	}
 
-	onDismiss(id) {
+	onSearchSubmit = (event) => {
+		const { searchTerm } = this.state
+		this.fetchSearchTopStories(searchTerm)
+		event.preventDefault()
+	}
+
+	onDismiss = (id) => {
 		const isNotId = item => item.objectID !== id
-		const updatedHits = this.state.result.hits.filter(isNotId)		
-		const updatedResult = {...this.state.result, hits: updatedHits }
-		this.setState({ result: updatedResult })
+		const updatedHits = this.state.result.hits.filter(isNotId)
+		this.setState({
+			result: { ...this.state.result, hits: updatedHits }
+		})
 	}
 
 	render() {
 		const { searchTerm, result } = this.state
-
 		return (
 			<div className="page">
 				<div className="interactions">
 					<Search
 						value={searchTerm}
 						onChange={this.onSearchChange}
+						onSubmit={this.onSearchSubmit}
 					>
             			Search
 					</Search>
 				</div>
-				{
-					result 
-						? <Table
-							list={result.hits}
-							pattern={searchTerm}
-							onDismiss={this.onDismiss}
-						/>
-						: null
+				{ result &&
+          <Table
+          	list={result.hits}
+          	onDismiss={this.onDismiss}
+          />
 				}
 			</div>
 		)
 	}
 }
 
-const Search = ({ value, onChange, children }) =>
-	<form>
-		{children} <input
+
+const Search = ({
+	value,
+	onChange,
+	onSubmit,
+	children
+}) =>
+	<form onSubmit={onSubmit}>
+		<input
 			type="text"
 			value={value}
 			onChange={onChange}
 		/>
+		<button type="submit">
+			{children}
+		</button>
 	</form>
 
-const Table = ({ list, pattern, onDismiss }) =>
+const Table = ({ list, onDismiss }) =>
 	<div className="table">
-		{ 
-			list.filter(isSearched(pattern)).map(item =>
-				<div key={item.objectID} className="table-row">
-					<span style={{ width: '40%' }}>
-						<a href={item.url}>{item.title}</a>
-					</span>
-					<span style={{ width: '30%' }}>
-						{item.author}
-					</span>
-					<span style={{ width: '10%' }}>
-						{item.num_comments}
-					</span>
-					<span style={{ width: '10%' }}>
-						{item.points}
-					</span>
-					<span style={{ width: '10%' }}>
-						<Button
-							onClick={() => onDismiss(item.objectID)}
-							className="button-inline"
-						>
-							Dismiss
-						</Button>
-					</span>
-				</div>
-			)
-		}
+		{ list.map(item =>
+			<div key={item.objectID} className="table-row">
+				<span style={{ width: '40%' }}>
+					<a href={item.url}>{item.title}</a>
+				</span>
+				<span style={{ width: '30%' }}>
+					{item.author}
+				</span>
+				<span style={{ width: '10%' }}>
+					{item.num_comments}
+				</span>
+				<span style={{ width: '10%' }}>
+					{item.points}
+				</span>
+				<span style={{ width: '10%' }}>
+					<Button
+						onClick={() => onDismiss(item.objectID)}
+						className="button-inline"
+					>
+            			Dismiss
+					</Button>
+				</span>
+			</div>
+		)}
 	</div>
 
 const Button = ({ onClick, className = '', children }) =>
