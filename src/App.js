608100c944a37f8624de9808c7881ad0d4fe92ea
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-10-21 09:31:42 -0600

    Conditional Rendering.

--- src/App.js
+++ src/App.js
@@ -57,8 +57,6 @@ class App extends Component {
 	render() {
 		const { searchTerm, result } = this.state
 
-		if (!result) return null
-
 		return (
 			<div className="page">
 				<div className="interactions">
@@ -69,11 +67,15 @@ class App extends Component {
             			Search
 					</Search>
 				</div>
-				<Table
-					list={result.hits}
-					pattern={searchTerm}
-					onDismiss={this.onDismiss}
-				/>
+				{
+					result 
+						? <Table
+							list={result.hits}
+							pattern={searchTerm}
+							onDismiss={this.onDismiss}
+						/>
+						: null
+				}
 			</div>
 		)
 	}
