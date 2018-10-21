d9ae22148d4f53581bc1172385bc1f6a9065bdca
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-10-20 18:15:03 -0600

    Local Component State.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import App from './App.js'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<App />,
+	document.getElementById('content')
+)
+
+if (module.hot) {
+	module.hot.accept()
+}
